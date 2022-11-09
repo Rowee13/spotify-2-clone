4ef10a1fb7199b4105ca54657765b1716ba667f7
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-11-09 22:05:47 +0800

    Added Discovery song listing and fetch data thru rapidapi shazamcore

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "0668f7ab07mshf262d41db18ff1cp1bce2cjsnf1a6e17ac067"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
